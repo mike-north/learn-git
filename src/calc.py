3aebf0ca868ef225bd153b9e1d51b3916032f4e5
Mike North <michael.l.north@gmail.com>
2021-02-07 16:09:11 -0800

    Create calc.py

---
+++ src/calc.py
@@ -0,0 +1,11 @@
+def add(a, b):  return a + b
+def sub(a, b):  return a - b
+def mult(a, b): return a * b
+def div(a, b):  return a / b
+
+
+def main(): print('Result = {}'.format(
+    add(3, sub(7, 2)))
+)
+
+main()
